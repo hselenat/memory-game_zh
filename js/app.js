8b51da2a259596d76dd0ed6c88b21bc0850a0e86
HSelenat <942430793@qq.com>
2018-03-31 22:19:11 +0800

    更新星星等级

--- js/app.js
+++ js/app.js
@@ -19,22 +19,18 @@ var cards = [
     '<li class="card"><i class="fa fa-paper-plane-o"></i></li>',
     '<li class="card"><i class="fa fa-cube"></i></li>'
 ];
-var open = [];
-var moves = 0;
-/*
- * 显示页面上的卡片
- *   - 使用下面提供的 "shuffle" 方法对数组中的卡片进行洗牌
- *   - 循环遍历每张卡片，创建其 HTML
- *   - 将每张卡的 HTML 添加到页面
- */
+let open = [];//定义变量为open的空数组，存放状态为 open显示的卡片
+let moves = 0;
+let stars = 3;
 
 function show(){
-    shuffle(cards).forEach(function (card) {
-        $('.deck').append(card);
+    shuffle(cards).forEach(function (card) {     //使用shuffle(cards)对数组中每个元素进行洗牌
+        $('.deck').append(card);                 //将遍历的每个元素添加到html的deck类中
     });
 
 }
-show();
+show();//显示出来
+
 // 洗牌函数来自于 http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -58,19 +54,26 @@ function shuffle(array) {
  *    + 增加移动计数器并将其显示在页面上（将这个功能放在你从这个函数中调用的另一个函数中）
  *    + 如果所有卡都匹配，则显示带有最终分数的消息（将这个功能放在你从这个函数中调用的另一个函数中）
  */
+
 function showCard() {
-    this.className = 'card open show';
+    this.className = 'card open show'; //显示卡片的符号 showCard()
 }
-function check() {
-    if (open.length == 2){
-        moves++;
-        checkMatch();
-    }
+
+$('.deck').on('click', 'li', function () {//点击deck类中的每个li执行以下操作
+    showCard.call(this);                  //显示所点击的this卡片
+    open.push(this);                      //添加进数组
+    check();
+});
+$('div.restart').on('click', restart);
+
+
+function check() {           //检查卡片
     updateMoves();
     checkWin();
     checkStars();
 }
-function checkMatch() {
+
+function checkMatch() {     //检查对比卡片
     if (open[0].firstChild.className == open[1].firstChild.className) {
         open.forEach(function (card) {
             card.className = 'card match';
@@ -85,11 +88,33 @@ function checkMatch() {
         },500);
     }
 }
+//更新星级
+function updateStars() {
+    if (moves <= 16) {
+        $('.stars .fa').addClass("fa-star");
+        stars = 3;
+    } else if (moves >= 17 && moves <= 25) {
+        $('.stars li:last-child .fa').removeClass("fa-star");
+        $('.stars li:last-child .fa').addClass("fa-star-o");
+        stars = 2;
+    } else {
+        $('.stars li:nth-child(2) .fa').removeClass("fa-star");
+        $('.stars li:nth-child(2) .fa').addClass("fa-star-o");
+        stars = 1;
+    }
+    $('.win-container .stars-number').text(stars);
+}
+
+//更新步数
 function updateMoves() {
+    if (open.length == 2) {
+        moves++;
+        checkMatch();
+    }
     $('span.moves').text(String(moves));
 }
 function checkWin() {
-    if($('.card.match').length == 2){
+    if($('.card.match').length == 4){
         if(confirm('again?')){
             restart();
         } else {
@@ -99,15 +124,18 @@ function checkWin() {
 }
 //重新开始游戏
 function restart() {
+    //清空所有卡片
     $('.deck li').remove();
+    //初始化星级
+    stars = 3;
+    $('.stars i').removeClass("fa-star-o");
+    $('.stars i').addClass("fa-star");
+    updateStars();
+    //初始化步数
     moves = 0;
     $('span.moves').text(String(moves));
+    //重新打乱并加载卡片
     show();
+    
 }
 
-$('.deck').on('click','li',function () {
-    showCard.call(this);//反转卡片
-    open.push(this);//添加进数组
-    check();
-});
-$('div.restart').on('click',restart);
